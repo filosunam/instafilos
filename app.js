d6c7bd460b3ed25a3d59d246fed59c1a35ec77a5
Marco Godínez <markotom@gmail.com>
2013-06-06 02:45:37 -0500

    Working for multiple locations ID around to specific coordinates

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ requirejs(['conf', 'express'], function(conf, express){
   var app     = express()
     , server  = app.listen(process.env.PORT || conf.server.port)
     , io      = require('socket.io').listen(server)
-    , ig      = require('instagram-node-lib');
+    , ig      = require('instagram-node-lib')
+    , _       = require('underscore');
 
   // set config instafilos app
   ig.set('client_id', conf.instagram.client_id);
@@ -53,32 +54,80 @@ requirejs(['conf', 'express'], function(conf, express){
   // socket.io
   io.sockets.on('connection', function (socket) {
 
-    // send images
-    ig.locations.recent({ 
-        location_id: 1167187
-      , count: 100
+    var images    = []
+      , locations = [];
+
+    // search locations around the school
+    ig.locations.search({
+        lat: 19.3341866760000001
+      , lng: -99.186748266000001
       , complete: function(data, resource){
-          socket.emit('recents', {
-              data: data
-            , resource: resource
+          // for each location ID
+          data.forEach(function(location, index){
+            
+            getImagesFromLocation(location, index, data);
+
           });
         }
     });
 
+    var getImagesFromLocation = function(location, index, data){
+
+      ig.locations.recent({
+          location_id: location.id
+        , max_id: location.max_id
+        , complete: function(d, r){
+
+            // for push more images to browser
+            locations.push({
+                location: location
+              , max_id: r.next_max_id
+            });
+
+            // for each bulk of images
+            d.forEach(function(image, i){
+
+              // push to all images
+              images.push(image);
+              
+              if (index === data.length - 1 && i === d.length - 1) {
+                
+                // sort by date descending
+                images  = _.sortBy(images, 'created_time').reverse();
+
+                // push images to browser
+                socket.emit('recents', {
+                    data: images
+                  , locations: locations
+                });
+
+                console.log('Send images:', images.length);
+
+              }
+
+            });
+          }
+      });
+    };
+
+
     // send more images, push, push, push...
     socket.on('more', function(data) {
-      ig.locations.recent({
-        location_id: 1167187
-      , count: 100
-      , max_id: data.max_id
-      , complete: function(data, resource){
-          socket.emit('recents', {
-              data: data
-            , resource: resource
-          });
-          console.log('Send data:', data.length);
-        }
+
+      /*
+      data = data.max_ids.split(',');
+      data.forEach(function(location, index){
+        location = location.split(':');
+        location = {
+            id     : location[0]
+          , max_id : location[1]
+        };
+        console.log(location);
+        getImagesFromLocation(location, index, data);
+      
       });
+      */
+
     });
 
   });
